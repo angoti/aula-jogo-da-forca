bb0d22aa89b2a2cc3a5ae9f130babebcdac87f79
angoti <angoti@gmail.com>
2025-09-01 12:00:41 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import { View, Image, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Image source={require('./assets/forca.png')} />
+    </View>
+  );
+}
