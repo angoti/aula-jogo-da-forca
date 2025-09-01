df245563c25d9b9f08bd0cdf57a6439b055ec564
angoti <angoti@gmail.com>
2025-09-01 12:18:16 -0300

    botão e useState

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Button } from 'react-native';
+import { useState } from 'react';
 
+const listaDeImagens = [
+  require('./assets/forca.png'),
+  require('./assets/cabeca.png'),
+  require('./assets/cabecacorpo.png'),
+  require('./assets/cabecacorpoumbraco.png'),
+  require('./assets/cabecacorpodoisbracos.png'),
+  require('./assets/cabecacorpodoisbracosumaperna.png'),
+  require('./assets/cabecacorpodoisbracosduaspernas.png'),
+];
 export default function App() {
+  // var faseDoJogo = 0;
+  const [faseDoJogo, setFaseDoJogo] = useState(0);
+
   return (
     <View>
-      <Image source={require('./assets/forca.png')} />
+      <Image source={listaDeImagens[faseDoJogo]} />
+      <Button
+        title="Jogar"
+        onPress={() => {
+          setFaseDoJogo(faseDoJogo + 1);
+          // faseDoJogo++;
+          // console.log(faseDoJogo)
+        }}
+      />
     </View>
   );
 }
